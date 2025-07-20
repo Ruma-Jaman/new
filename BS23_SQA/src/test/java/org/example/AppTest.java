fe33874bd0b37f59687929866da6ccd7a3f66307
Ruma-Jaman <rumajaman308@gmail.com>
2025-07-20 11:58:13 +0600

    nopCommerce Automation

--- BS23_SQA/src/test/java/org/example/AppTest.java
+++ BS23_SQA/src/test/java/org/example/AppTest.java
@@ -1,11 +1,13 @@
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.FileReader;
-import java.util.List;
+import java.time.Duration;
 
 public class AppTest {
     public static void main(String[] args) throws Exception {
